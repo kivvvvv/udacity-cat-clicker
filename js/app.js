c591590fca08365ac2e687bf2c89c048a75e2190
kivvvvv <kivistheslayer@gmail.com>
2019-04-20 14:23:21 +0700

    Prepare the 'catImage' object

--- js/app.js
+++ js/app.js
@@ -3,11 +3,36 @@ const imgCatEl = document.querySelector("#cat");
 const countEl = document.querySelector("#count");
 
 const catImages = [
-  { catName: "Chewie", url: "./img/cat-chewie.jpg" },
-  { catName: "Max", url: "./img/kitten-max.jpg" },
-  { catName: "Tigger", url: "./img/cat-tigger.jpg" },
-  { catName: "Tom", url: "./img/cat-tom.jpg" },
-  { catName: "Xuxa", url: "./img/kitten-xuxa.jpg" }
+  {
+    catName: "Chewie",
+    url: "./img/cat-chewie.jpg",
+    pictureName: "Chewie the Cat",
+    clickCount: 0
+  },
+  {
+    catName: "Max",
+    url: "./img/kitten-max.jpg",
+    pictureName: "Little Max",
+    clickCount: 0
+  },
+  {
+    catName: "Tigger",
+    url: "./img/cat-tigger.jpg",
+    pictureName: "Tigger the Cat",
+    clickCount: 0
+  },
+  {
+    catName: "Tom",
+    url: "./img/cat-tom.jpg",
+    pictureName: "Tom the Cat",
+    clickCount: 0
+  },
+  {
+    catName: "Xuxa",
+    url: "./img/kitten-xuxa.jpg",
+    pictureName: "Little Xuxa",
+    clickCount: 0
+  }
 ];
 
 listCatEl.addEventListener("click", e => {
